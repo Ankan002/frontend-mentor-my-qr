9964dec11ec4407b55f5cc80e84b960bd2bc5130
ankanbhattacharya <ankanbhattachatya89@gmail.com>
2022-07-03 16:11:21 +0530

    build: :rocket: v1.0.0

---
+++ readme.md
@@ -0,0 +1,15 @@
+# My QR
+
+Website: [My QR](https://my-qr.netlify.app)
+
+## Screenshot
+
+<img src="/assets/scrrenshot.png" />
+
+## Technologies
+
+- HTML
+
+- Javascript
+
+- TailwindCSS
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-grey": "#B7B8BD",
+        "grayish-blue": "#D5E0EF",
+        "dark-blue": "#384251"
+      },
+      fontFamily: {
+        "outfit": "'Outfit', sans-serif"
+      }
+    },
   },
   plugins: [],
 }
